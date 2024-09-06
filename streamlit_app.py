8f2f44969e88d90409a17dd617cd1d7369ceebcf
Febriano Bramantyo Harahap <febriano.bramantyo@live.com>
2024-09-06 21:24:27 +0000

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,21 @@
 import streamlit as st
 from streamlit_gsheets import GSheetsConnection
 
+spreadsheet_master = "https://docs.google.com/spreadsheets/d/1WYwhKtns9Jd0QZ4QmlJOd_YO9baQG5sBLGeBcf-hJMY"
+spreadsheet_response = "https://docs.google.com/spreadsheets/d/1PbqBhlvhcIFN7i-19vVZqEkYX45s07mcgn3QVyUQdjQ"
+
 # Create a connection object.
 conn = st.connection("gsheets", type=GSheetsConnection)
 
-df = conn.read(worksheet="Question")
+df_question = conn.read(spreadsheet=spreadsheet_master, worksheet="Question")
+df_response_header = conn.read(spreadsheet=spreadsheet_response, worksheet="Response - Header")
 
 # Print results.
-for row in df.itertuples():
-    st.write(f"{int(row.Id)} - {row.Question}")
+# for row in df_question.itertuples():
+#     st.write(f"{int(row.Id)} - {row.Question}")
+
+for row in df_response_header.itertuples():
+    st.write(f"{int(row.Id)} - {row.Date} - {row.Person} - {row.Question}")
 
 
 # st.title("🎈 My new app")
