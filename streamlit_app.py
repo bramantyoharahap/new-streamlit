fcc9e28df03f5ad6822ea9f924ab21ba8d5d1ca5
Febriano Bramantyo <febriano.bramantyo@ibm.com>
2024-09-08 03:07:24 +0700

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 from streamlit_gsheets import GSheetsConnection
+import pandas as pd
+from pandasql import sqldf
 
 spreadsheet_master = "https://docs.google.com/spreadsheets/d/1WYwhKtns9Jd0QZ4QmlJOd_YO9baQG5sBLGeBcf-hJMY"
 spreadsheet_response = "https://docs.google.com/spreadsheets/d/1PbqBhlvhcIFN7i-19vVZqEkYX45s07mcgn3QVyUQdjQ"
@@ -8,17 +10,24 @@ spreadsheet_response = "https://docs.google.com/spreadsheets/d/1PbqBhlvhcIFN7i-1
 conn = st.connection("gsheets", type=GSheetsConnection)
 
 df_question = conn.read(spreadsheet=spreadsheet_master, worksheet="Question")
+df_option = conn.read(spreadsheet=spreadsheet_master, worksheet="Option")
 df_response_header = conn.read(spreadsheet=spreadsheet_response, worksheet="Response - Header")
+df_response_detail = conn.read(spreadsheet=spreadsheet_response, worksheet="Response - Detail")
+df_sale = conn.read(spreadsheet=spreadsheet_master, worksheet="Test")
 
-# Print results.
-# for row in df_question.itertuples():
-#     st.write(f"{int(row.Id)} - {row.Question}")
+response_data = pd.merge(left= df_response_header, right=df_response_detail, left_on="Id", right_on="ResponseId")
+response_data = pd.merge(left= response_data, right=df_option, left_on="Option", right_on="Id")
+response_data['Grade'] = response_data['Grade'].astype(int)
 
-for row in df_response_header.itertuples():
-    st.write(f"{int(row.Id)} - {row.Date} - {row.Person} - {row.Question}")
-
-
-# st.title("🎈 My new app")
-# st.write(
-#     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-# )
+for i in range(1,30):
+    _df = sqldf(f"""            
+        Select 
+            Person,
+            cast(Grade as varchar) as Rank,  
+            Sequence,
+            (select count(distinct person) from df_response_header) as Responder
+        from response_data where Question = {i}
+        """)
+    st.bar_chart(_df, x="Sequence", y="Responder", color="Rank", stack="normalize")
+    
+    #("#c0e6f5","#b5e6a2","#daf2d0", "#ffff00", "#ff0000")
